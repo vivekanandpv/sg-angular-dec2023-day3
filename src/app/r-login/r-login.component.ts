2fb6597b52c7891d1e20fc35d6799a4661eb7e6b
Vivekanand P V <vivekanandpv@gmail.com>
2023-12-06 16:19:19 +0530

    done

--- src/app/r-login/r-login.component.ts
+++ src/app/r-login/r-login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-r-login',
@@ -16,8 +19,11 @@ export class RLoginComponent {
 
   constructor(fb: FormBuilder) {
     this.form = fb.group({
-      username: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required, Validators.minLength(8)]],
+      username: [
+        null,
+        [Validators.required, Validators.email, this.validateSync.bind(this)],
+      ],
+      password: [null, [Validators.required], [this.validateAsync.bind(this)]],
       checkMeOut: [false],
     });
   }
@@ -32,6 +38,30 @@ export class RLoginComponent {
 
   get checkMeOut(): FormControl {
     return this.form.controls['checkMeOut'] as FormControl;
+  }
+
+  validateSync(ctrl: AbstractControl): null | ValidationErrors {
+    const value = ctrl.value;
+
+    if (value && value.length < 8) {
+      return {
+        my_error: true,
+      };
+    } else {
+      return null;
+    }
+  }
+
+  validateAsync(ctrl: AbstractControl): Observable<null | ValidationErrors> {
+    const value = ctrl.value;
+
+    if (value && value.length < 8) {
+      return of({
+        my_async_error: true,
+      });
+    } else {
+      return of(null);
+    }
   }
 
   onSubmit() {
